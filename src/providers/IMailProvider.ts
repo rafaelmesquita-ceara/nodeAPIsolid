9e2f5a42ee90378f1bc533f593669192d02d8bf7
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-09-11 00:54:27 -0300

    Feita a interface de envio de email IMailProvider.ts

---
+++ src/providers/IMailProvider.ts
@@ -0,0 +1,15 @@
+interface IAddress{
+  email: string;
+  name: string;
+}
+
+export interface IMessage{
+  to: string;
+  from: string;
+  subject: string;
+  body: string;
+}
+
+export interface IMailProvider {
+  sendMail(message: IMessage): Promise<void>
+}
