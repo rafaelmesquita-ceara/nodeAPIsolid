69079d946c7cb3b3529a42b13f7c19d67808074e
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-09-11 00:48:14 -0300

    CreateUserUseCase configurado

--- src/repositories/IUsersRepository.ts
+++ src/repositories/IUsersRepository.ts
@@ -1,6 +1,6 @@
 import { User } from "../entities/User";
 
 export interface IUsersRepository {
-  findByEmail(email: string): Promise<User>;
-  save(user: User): Promise<void>;
+  findByEmail(email: string): Promise<User>; //Função que recebe email por parâmetro e retorna um User
+  save(user: User): Promise<void>;  //Função que recebe User por parâmetro e não retorna nada
 }
--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,5 +1,23 @@
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+
 export class CreateUserUseCase{
-  async execute(){
+  constructor(
+    private usersRepository: IUsersRepository // private para não precisar fazer o this.usersRepository
+  ){
     
+  }
+
+
+  async execute(data: ICreateUserRequestDTO){ // data recebe o tipo de data pelo DTO
+    const userAlreadyExists = await this.usersRepository.findByEmail(data.email); // Verifica se o email já existe
+    if (userAlreadyExists) {
+      throw new Error('User already exists.'); // Se sim, ele estoura um erro
+    }
+
+    const user = new User(data); // Recebe o usuario pelo DTO
+
+    await this.usersRepository.save(user); // Salva o usuario no meu método save
   }
 }
