f22f6e3194fc55a7e0ddec784f4992b8446632c9
Rafael Mesquita <rafaelempresarialacc@gmail.com>
2020-09-11 01:03:34 -0300

    Controller do User criado

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -3,16 +3,16 @@ import { IMailProvider } from "../../providers/IMailProvider";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 
-export class CreateUserUseCase{
+export class CreateUserUseCase {
   constructor(
     private usersRepository: IUsersRepository, // private para não precisar fazer o this.usersRepository
-    private mailProvider : IMailProvider
-  ){
-    
+    private mailProvider: IMailProvider
+  ) {
+
   }
 
 
-  async execute(data: ICreateUserRequestDTO){ // data recebe o tipo de data pelo DTO
+  async execute(data: ICreateUserRequestDTO) { // data recebe o tipo de data pelo DTO
     const userAlreadyExists = await this.usersRepository.findByEmail(data.email); // Verifica se o email já existe
     if (userAlreadyExists) {
       throw new Error('User already exists.'); // Se sim, ele estoura um erro
@@ -24,15 +24,15 @@ export class CreateUserUseCase{
 
     this.mailProvider.sendMail({
       to: {
-        name : data.name,
-        email : data.email
+        name: data.name,
+        email: data.email
       },
       from: {
-        name : 'Equipe do meu App',
-        email : 'equipe@meuapp.com',
+        name: 'Equipe do meu App',
+        email: 'equipe@meuapp.com',
       },
-      subject : 'Seja bem-vindo à plataforma',
-      body : '<p>Você já pode fazer login em nossa plataforma</p>'
+      subject: 'Seja bem-vindo à plataforma',
+      body: '<p>Você já pode fazer login em nossa plataforma</p>'
     }
     );
   }
